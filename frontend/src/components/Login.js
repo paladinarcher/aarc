653c32b05ee0aabcb67cb81d35d66df83571fb40
webpromo <rank_booster@yahoo.com>
2019-01-08 10:12:59 -0700

    Axios installed

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,65 +3,115 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class Login extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+            username: "",
+            password: ""
+        }
+    }
+
+    updateInfo(updatedInfo,target) {
+        this.setState({
+            [target]:updatedInfo
+        })
+    }
+    
+    onClick() {
+        /* attach a submit handler to the form */
+        // $("#at-pwd-form").submit(function (event) {
+    
+        //   /* stop form from submitting normally */
+        //   event.preventDefault();
+    
+        //   /* get the action attribute from the <form action=""> element */
+        //   var $form = $(this),
+        //     url = $form.attr('action');
+    
+        //   /* Send the data using post */
+        //   var posting = $.post(
+        //     url,
+        //     {
+        //       username: $('#username').val(),
+        //       password: $('#password').val(),
+        //       loggedIn: true
+        //     },
+        //     function (data, status, jqXHR) {
+        //       alert("AuthAPI Token: \n" + data.token) // access the user token from here.
+        //     },
+        //     "json"
+        //   );
+    
+        //   /* redirects once logged in */
+        //   posting.done(function (data) {
+        //     window.location.assign("/dashboard");
+        //   });
+        // });
+    }
+
   render() {
     return (
 
         // <!-- login forms -->
-        <div class="col-sm-5 col-sm-pull-6 align-middle atform">
+        <div className="col-sm-5 col-sm-pull-6 align-middle atform">
 
-            <div class="at-form">
+            <div className="at-form">
 
-                <div class="at-title">
+                <div className="at-title">
                     <h3>Sign In</h3>
                 </div>
 
-                <div class="at-pwd-form">
-                    <form id="at-pwd-form" novalidate="" 
-                    action="http://localhost:3001/users/authenticate" method="POST">
+                <div className="at-pwd-form">
+                    <form id="at-pwd-form" noValidate="" 
+                    action="http://localhost:8888/api/v1/login" method="POST">
                         <fieldset>
 
-                            <div class="at-input form-group">
-                                <label class="control-label" for="username">
+                            <div className="at-input form-group">
+                                <label className="control-label" htmlFor="username">
                                     Email
                                 </label>
-                                <input type="email" class="form-control" id="username" name="username"
-                                    autocapitalize="none" autocorrect="off"></input>
+                                <input type="email" className="form-control" id="username" name="username"
+                                    autoCapitalize="none" autoCorrect="off" 
+                                    onChange={(e) => this.updateInfo(e.target.value,e.target.id)} value= {this.state.addy}></input>
 
-                                <span class="help-block hide"></span>
+                                <span className="help-block hide"></span>
                             </div>
 
 
-                            <div class="at-input form-group">
-                                <label class="control-label" for="password">
+                            <div className="at-input form-group">
+                                <label className="control-label" htmlFor="password">
                                     Password
                                 </label>
-                                <input type="password" class="form-control" id="password" name="at-field-password"
-                                    autocapitalize="none" autocorrect="off"></input>
+                                <input type="password" className="form-control" id="password" name="at-field-password"
+                                    autoCapitalize="none" autoCorrect="off"
+                                    onChange={(e) => this.updateInfo(e.target.value,e.target.id)} value= {this.state.pass}>
+                                    </input>
 
-                                <span class="help-block hide"></span>
+                                <span className="help-block hide"></span>
                             </div>
 
 
-                            <div class="at-pwd-link">
+                            <div className="at-pwd-link">
                                 <p>
 
-                                    <a href="#/forgot" id="at-forgotPwd" class="at-link at-pwd">Forgot your
+                                    <a href="#/forgot" id="at-forgotPwd" className="at-link at-pwd">Forgot your
                                         password?</a>
 
                                 </p>
                             </div>
 
-                            <button type="submit" class="at-btn submit btn btn-lg btn-block btn-default" id="at-btn">
+                            <button type="submit" className="at-btn submit btn btn-lg btn-block btn-default" id="at-btn">
                                 Sign In
                             </button>
                         </fieldset>
                     </form>
                 </div>
 
-                <div class="at-signup-link">
+                <div className="at-signup-link">
                     <p>
                         Don't have an account?&nbsp;
-                        <a href="#/register" id="at-signUp" class="at-link at-signup">Register</a>
+                        <a href="#/register" id="at-signUp" className="at-link at-signup">Register</a>
                     </p>
                 </div>
 
