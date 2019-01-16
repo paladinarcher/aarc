083cb79bd5c26412f223bd1289a2a669d5597952
Richard Memory <richardfmemory@gmail.com>
2019-01-15 20:17:44 -0700

    add a chmod instruction to execute docker-build.sh: take 3

--- docker/jenkins/docker-build.sh
+++ docker/jenkins/docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/src/aarc/backend/docker
+cd /home/ec2-user/src/aarc/production
 sudo /usr/local/bin/docker-compose down
 sudo /usr/local/bin/docker-compose build
 sudo /usr/local/bin/docker-compose up -d
