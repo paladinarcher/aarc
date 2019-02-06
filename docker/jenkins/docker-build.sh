c718622cd2db7bc668f0318ab58e294e8f328e91
Richard Memory <richardfmemory@gmail.com>
2019-02-05 17:03:57 -0700

    docker-build path change

--- docker/jenkins/docker-build.sh
+++ docker/jenkins/docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/src/aarc/production/fireDrum
+cd /home/ec2-user/src/aarc/fireDrum/production
 sudo /usr/local/bin/docker-compose down
 sudo /usr/local/bin/docker-compose build
 sudo /usr/local/bin/docker-compose up -d
